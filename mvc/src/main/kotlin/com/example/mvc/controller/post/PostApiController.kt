7ca7609a99b708d0df6b3ace7582a55ad5c6a123
tychejin <tychejin@naver.com>
2022-04-22 18:48:50 +0900

    Feat: REST API - POST

---
+++ mvc/src/main/kotlin/com/example/mvc/controller/post/PostApiController.kt
@@ -0,0 +1,27 @@
+package com.example.mvc.controller.post
+
+import com.example.mvc.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.function.RequestPredicates.POST
+
+
+@RestController
+@RequestMapping("/api")
+class PostApiController {
+
+    @PostMapping("/post-mapping")
+    fun postMapping(): String {
+        return "post-mapping"
+    }
+
+    @RequestMapping(method = [RequestMethod.POST], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    @PostMapping("/post-mapping/object")
+    fun postMappingObject(@RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+}
--- mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,10 +1,15 @@
 package com.example.mvc.model.http
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest(
     var name: String? = null,
     var age: Int? = null,
     var email: String? = null,
     var address: String? = null,
     //var phone-number: String? = null : 변수 선언 시 하이픈을 사용할 수 없음
+    //@JsonProperty("phone_number")
     var phoneNumber: String? = null
 )
