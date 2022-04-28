c6da76e96516ca9fdd65554528508c8d5d4957ef
A-Bong <tychejin@naver.com>
2022-04-28 15:36:49 +0900

    Feat: 예외처리 - ControllerAdvice

---
+++ mvc/src/main/kotlin/com/example/mvc/controller/exception/ExceptionApiController.kt
@@ -0,0 +1,28 @@
+package com.example.mvc.controller.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/exception")
+class ExceptionApiController {
+
+    @GetMapping("/hello")
+    fun hello() {
+        val list = mutableListOf<String>()
+        val temp = list[0]
+        /*if (true) {
+            throw RuntimeException("강제 exception 발생")
+        }*/
+    }
+
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun IndexOutOfBoundsException(e: IndexOutOfBoundsException): ResponseEntity<String> {
+        println("controller exception handler")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
+    }
+}
