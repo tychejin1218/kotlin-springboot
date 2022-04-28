eae7f9fb8acd582920a9597f8fe9e9f779b27dc9
A-Bong <tychejin@naver.com>
2022-04-28 15:00:58 +0900

    Feat: Spring Boot Validation - Custom Validation Annotation 만들어서 사용하기

--- mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ mvc/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,5 +1,6 @@
 package com.example.mvc.model.http
 
+import com.example.mvc.annotation.StringFormatDateTime
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.time.LocalDateTime
@@ -27,9 +28,10 @@ data class UserRequest(
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")
     var phoneNumber: String? = null,
 
+    @field:StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "패턴이 올바르지 않습니다.")
     var createdAt: String? = null // yyyy-MM-dd HH:mm:ss ex) 2022-04-08 13:00:00
 ) {
-    @AssertTrue(message = "생성일자의 패턴은 yyyy-mm-dd HH:mm:ss 여야 합니다")
+    /*@AssertTrue(message = "생성일자의 패턴은 yyyy-mm-dd HH:mm:ss 여야 합니다")
     private fun isValidCreateAt():Boolean { // 정상 true, 비정상 false
         return try {
             LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
@@ -37,5 +39,5 @@ data class UserRequest(
         } catch (e: Exception) {
             false
         }
-    }
+    }*/
 }
